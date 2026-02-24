b6a7c9a7328be3a16f6a2bcea4b2857e1d5a3208
Junior Sundar <juniorsundar@gmail.com>
2026-02-24 21:00:23 +0400

    chore: stylua

--- tests/refer/commands_preview_spec.lua
+++ tests/refer/commands_preview_spec.lua
@@ -7,43 +7,43 @@ describe("builtin.commands preview", function()
     before_each(function()
         buf = vim.api.nvim_create_buf(false, true)
         vim.api.nvim_win_set_buf(0, buf)
-        vim.api.nvim_buf_set_lines(buf, 0, -1, false, {"foo", "foo", "foo"})
+        vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "foo", "foo", "foo" })
     end)
 
     after_each(function()
         if picker then
             picker:close()
         end
-        pcall(vim.api.nvim_buf_delete, buf, {force=true})
+        pcall(vim.api.nvim_buf_delete, buf, { force = true })
     end)
 
     it("applies substitute preview on change", function()
         picker = builtin.commands()
         picker.opts.on_change("%s/foo/bar/g", function() end)
-        
+
         local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-        assert.are.same({"bar", "bar", "bar"}, lines)
+        assert.are.same({ "bar", "bar", "bar" }, lines)
     end)
 
     it("works with range prefix", function()
         picker = builtin.commands()
         picker.opts.on_change("1,2s/foo/bar/g", function() end)
-        
+
         local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-        assert.are.same({"bar", "bar", "foo"}, lines)
+        assert.are.same({ "bar", "bar", "foo" }, lines)
     end)
 
     it("undoes preview on close", function()
         picker = builtin.commands()
         picker.opts.on_change("1,2s/foo/bar/g", function() end)
-        
+
         local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-        assert.are.same({"bar", "bar", "foo"}, lines)
+        assert.are.same({ "bar", "bar", "foo" }, lines)
 
         picker:close()
 
         lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-        assert.are.same({"foo", "foo", "foo"}, lines)
+        assert.are.same({ "foo", "foo", "foo" }, lines)
     end)
 
     it("applies substitute preview for cdo s/foo/bar/g", function()
@@ -51,6 +51,6 @@ describe("builtin.commands preview", function()
         picker.opts.on_change("cdo s/foo/bar/g", function() end)
 
         local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-        assert.are.same({"bar", "bar", "bar"}, lines)
+        assert.are.same({ "bar", "bar", "bar" }, lines)
     end)
 end)
